e19867f63d848e0409c674e0d8f0708b7a471208
Peter Gao <petergao@berkeley.edu>
2017-12-10 04:40:43 +0000

    img id

--- dk_client.py
+++ dk_client.py
@@ -2,9 +2,10 @@ import grpc
 import model_serving_pb2
 import model_serving_pb2_grpc
 
+IMG_ID = 3 # image to predict, range [0,30)
 
 channel = grpc.insecure_channel('localhost:19999')
 stub = model_serving_pb2_grpc.ModelStub(channel)
 
-response = stub.Predict(model_serving_pb2.Input(img_id=3))
+response = stub.Predict(model_serving_pb2.Input(img_id=IMG_ID))
 print("Greeter client received: " + response.res)
